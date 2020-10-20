bf36abe4a726dff9722b0c3be7407991dd6999de
plainheart <yhen@all-my-life.cn>
2020-10-20 14:14:54 +0800

    chore: update probot and format code.

--- index.js
+++ index.js
@@ -1,243 +1,241 @@
-const Issue = require('./src/issue');
-const text = require('./src/text');
-const { isCoreCommitter } = require('./src/coreCommitters');
+const Issue = require('./src/issue')
+const text = require('./src/text')
+const { isCoreCommitter } = require('./src/coreCommitters')
 
 module.exports = app => {
-    app.on(['issues.opened'], async context => {
-        const issue = new Issue(context);
+  app.on(['issues.opened'], async context => {
+    const issue = new Issue(context)
 
-        // Ignore comment because it will commented when adding invalid label
-        const comment = issue.response === text.NOT_USING_TEMPLATE
-            ? Promise.resolve()
-            : commentIssue(context, issue.response);
+    // Ignore comment because it will commented when adding invalid label
+    const comment = issue.response === text.NOT_USING_TEMPLATE
+      ? Promise.resolve()
+      : commentIssue(context, issue.response)
 
-        const addLabels = issue.addLabels.length
-            ? context.github.issues.addLabels(context.issue({
-                labels: issue.addLabels
-            }))
-            : Promise.resolve();
+    const addLabels = issue.addLabels.length
+      ? context.github.issues.addLabels(context.issue({
+        labels: issue.addLabels
+      }))
+      : Promise.resolve()
 
-        const removeLabel = issue.removeLabel
-            ? getRemoveLabel(issue.removeLabel)
-            : Promise.resolve();
+    const removeLabel = issue.removeLabel
+      ? getRemoveLabel(issue.removeLabel)
+      : Promise.resolve()
 
-        return Promise.all([comment, addLabels, removeLabel]);
-    });
+    return Promise.all([comment, addLabels, removeLabel])
+  })
 
-    app.on('issues.labeled', async context => {
-        const replaceAt = function (comment) {
-            return replaceAll(
-                comment,
-                'AT_ISSUE_AUTHOR',
-                '@' + context.payload.issue.user.login
-            );
-        };
+  app.on('issues.labeled', async context => {
+    const replaceAt = function (comment) {
+      return replaceAll(
+        comment,
+        'AT_ISSUE_AUTHOR',
+        '@' + context.payload.issue.user.login
+      )
+    }
 
-        switch (context.payload.label.name) {
-            case 'invalid':
-                return Promise.all([commentIssue(context, text.NOT_USING_TEMPLATE), closeIssue(context)]);
+    switch (context.payload.label.name) {
+      case 'invalid':
+        return Promise.all([commentIssue(context, text.NOT_USING_TEMPLATE), closeIssue(context)])
 
-            case 'howto':
-                return Promise.all([commentIssue(context, text.LABEL_HOWTO), closeIssue(context)]);
+      case 'howto':
+        return Promise.all([commentIssue(context, text.LABEL_HOWTO), closeIssue(context)])
 
-            case 'inactive':
-                return Promise.all([commentIssue(context, text.INACTIVE_ISSUE), closeIssue(context)]);
+      case 'inactive':
+        return Promise.all([commentIssue(context, text.INACTIVE_ISSUE), closeIssue(context)])
 
-            case 'missing-demo':
-                return Promise.all([
-                    commentIssue(context, replaceAt(text.MISSING_DEMO)),
-                    getRemoveLabel(context, 'waiting-for: community'),
-                    context.github.issues.addLabels(context.issue({
-                        labels: ['waiting-for: author']
-                    }))
-                ]);
+      case 'missing-demo':
+        return Promise.all([
+          commentIssue(context, replaceAt(text.MISSING_DEMO)),
+          getRemoveLabel(context, 'waiting-for: community'),
+          context.github.issues.addLabels(context.issue({
+            labels: ['waiting-for: author']
+          }))
+        ])
 
-            // case 'waiting-for: author':
-            //     return commentIssue(context, replaceAt(text.ISSUE_TAGGED_WAITING_AUTHOR));
+        // case 'waiting-for: author':
+        //     return commentIssue(context, replaceAt(text.ISSUE_TAGGED_WAITING_AUTHOR));
 
-            case 'difficulty: easy':
-                return commentIssue(context, replaceAt(text.ISSUE_TAGGED_EASY));
+      case 'difficulty: easy':
+        return commentIssue(context, replaceAt(text.ISSUE_TAGGED_EASY))
 
-            case 'priority: high':
-                return commentIssue(context, replaceAt(text.ISSUE_TAGGED_PRIORITY_HIGH));
-        }
-    });
+      case 'priority: high':
+        return commentIssue(context, replaceAt(text.ISSUE_TAGGED_PRIORITY_HIGH))
+    }
+  })
 
-    app.on('issue_comment.created', async context => {
-        const isPr = context.payload.issue.html_url.indexOf('/pull/') > -1;
-        if (isPr) {
-            // Do nothing when pr is commented
-            return;
-        }
+  app.on('issue_comment.created', async context => {
+    const isPr = context.payload.issue.html_url.indexOf('/pull/') > -1
+    if (isPr) {
+      // Do nothing when pr is commented
+      return
+    }
 
-        const commenter = context.payload.comment.user.login;
-        const isCommenterAuthor = commenter === context.payload.issue.user.login;
-        let removeLabel;
-        let addLabel;
-        if (isCoreCommitter(commenter)) {
-            // New comment from core committers
-            removeLabel = getRemoveLabel(context, 'waiting-for: community');
-        }
-        else if (isCommenterAuthor) {
-            // New comment from issue author
-            removeLabel = getRemoveLabel(context, 'waiting-for: author');
-            addLabel = context.github.issues.addLabels(context.issue({
-                labels: ['waiting-for: community']
-            }));
-        }
-        return Promise.all([removeLabel, addLabel]);
-    });
+    const commenter = context.payload.comment.user.login
+    const isCommenterAuthor = commenter === context.payload.issue.user.login
+    let removeLabel
+    let addLabel
+    if (isCoreCommitter(commenter)) {
+      // New comment from core committers
+      removeLabel = getRemoveLabel(context, 'waiting-for: community')
+    } else if (isCommenterAuthor) {
+      // New comment from issue author
+      removeLabel = getRemoveLabel(context, 'waiting-for: author')
+      addLabel = context.github.issues.addLabels(context.issue({
+        labels: ['waiting-for: community']
+      }))
+    }
+    return Promise.all([removeLabel, addLabel])
+  })
 
-    app.on(['pull_request.opened'], async context => {
-        const isCore = isCoreCommitter(context.payload.pull_request.user.login);
-        let commentText = isCore
-            ? text.PR_OPENED_BY_COMMITTER
-            : text.PR_OPENED;
+  app.on(['pull_request.opened'], async context => {
+    const isCore = isCoreCommitter(context.payload.pull_request.user.login)
+    let commentText = isCore
+      ? text.PR_OPENED_BY_COMMITTER
+      : text.PR_OPENED
 
-        const labelList = [];
-        // const isDraft = !!context.payload.pull_request.draft;
-        // if (isDraft) {
-            labelList.push('PR: awaiting review');
-        // }
-        if (isCore) {
-            labelList.push('PR: author is committer');
-        }
+    const labelList = []
+    // const isDraft = !!context.payload.pull_request.draft;
+    // if (isDraft) {
+    labelList.push('PR: awaiting review')
+    // }
+    if (isCore) {
+      labelList.push('PR: author is committer')
+    }
 
-        const content = context.payload.pull_request.body;
-        if (content && content.indexOf('[x] The API has been changed.') > -1) {
-            labelList.push('PR: awaiting doc');
-            commentText += '\n\n' + text.PR_AWAITING_DOC;
-        }
+    const content = context.payload.pull_request.body
+    if (content && content.indexOf('[x] The API has been changed.') > -1) {
+      labelList.push('PR: awaiting doc')
+      commentText += '\n\n' + text.PR_AWAITING_DOC
+    }
 
-        const defaultBranch = context.payload.repository.default_branch;
-        const base = context.payload.pull_request.base;
-        if (base.ref !== defaultBranch) {
-            labelList.push('Branch: ' + base.ref);
-        }
+    const defaultBranch = context.payload.repository.default_branch
+    const base = context.payload.pull_request.base
+    if (base.ref !== defaultBranch) {
+      labelList.push('Branch: ' + base.ref)
+    }
 
-        const comment = context.github.issues.createComment(context.issue({
-            body: commentText
-        }));
-
-        const addLabel = context.github.issues.addLabels(context.issue({
-            labels: labelList
-        }));
-
-        return Promise.all([comment, addLabel]);
-    });
-
-    app.on(['pull_request.synchronize'], async context => {
-        const addLabel = context.github.issues.addLabels(context.issue({
-            labels: ['PR: awaiting review']
-        }));
-        const removeLabel = getRemoveLabel(context, 'PR: revision needed');
-        return Promise.all([addLabel, removeLabel]);
-    });
-
-    // seems no event for convert_to_draft
-    // app.on(['pull_request.ready_for_review'], async context => {
-    //     return context.github.issues.addLabels(context.issue({
-    //         labels: ['PR: awaiting review']
-    //     }));
-    // });
-
-    app.on(['pull_request.edited'], async context => {
-        const addLabels = [];
-        const removeLabels = [];
-
-        // const isDraft = !!context.payload.pull_request.draft
-        // if (isDraft) {
-        //     removeLabels.push(getRemoveLabel(context, 'PR: awaiting review'));
-        // }
-        // else {
-        //     addLabels.push('PR: awaiting review');
-        // }
-
-        const content = context.payload.pull_request.body;
-        if (content && content.indexOf('[x] The API has been changed.') > -1) {
-            addLabels.push('PR: awaiting doc');
-        }
-        else {
-            removeLabels.push(getRemoveLabel(context, 'PR: awaiting doc'));
-        }
-
-        const defaultBranch = context.payload.repository.default_branch;
-        const base = context.payload.pull_request.base;
-        const changedBase = context.payload.changes.base;
-        if (changedBase && changedBase.ref.from === defaultBranch && base.ref !== defaultBranch) {
-            addLabels.push('Branch: ' + base.ref);
-        }
-        if (changedBase && changedBase.ref.from !== defaultBranch) {
-            removeLabel.push(getRemoveLabel(context, 'Branch: ' + changedBase.ref.from));
-        }
-
-        const addLabel = context.github.issues.addLabels(context.issue({
-            labels: addLabels
-        }));
-
-        return Promise.all(removeLabels.concat([addLabel]));
-    });
-
-    app.on(['pull_request.closed'], async context => {
-        const actions = [
-            getRemoveLabel(context, 'PR: revision needed'),
-            getRemoveLabel(context, 'PR: awaiting review')
-        ];
-        const isMerged = context.payload['pull_request'].merged;
-        if (isMerged) {
-            const comment = context.github.issues.createComment(context.issue({
-                body: text.PR_MERGED
-            }));
-            actions.push(comment);
-        }
-        return Promise.all(actions);
-    });
-
-    app.on(['pull_request_review.submitted'], async context => {
-        if (context.payload.review.state === 'changes_requested'
-            && isCoreCommitter(context.payload.review.user.login)
-        ) {
-            const addLabel = context.github.issues.addLabels(context.issue({
-                labels: ['PR: revision needed']
-            }));
-
-            const removeLabel = getRemoveLabel(context, 'PR: awaiting review');
-            return Promise.all([addLabel, removeLabel]);
-        }
-    });
-}
-
-function getRemoveLabel(context, name) {
-    return context.github.issues.removeLabel(
-        context.issue({
-            name: name
-        })
-    ).catch(err => {
-        // Ignore error caused by not existing.
-        // if (err.message !== 'Not Found') {
-        //     throw(err);
-        // }
-    });
-}
-
-function closeIssue(context) {
-    const closeIssue = context.github.issues.update(context.issue({
-        state: 'closed'
-    }));
-    return closeIssue;
-}
-
-function commentIssue(context, commentText) {
     const comment = context.github.issues.createComment(context.issue({
-        body: commentText
-    }));
-    return comment;
+      body: commentText
+    }))
+
+    const addLabel = context.github.issues.addLabels(context.issue({
+      labels: labelList
+    }))
+
+    return Promise.all([comment, addLabel])
+  })
+
+  app.on(['pull_request.synchronize'], async context => {
+    const addLabel = context.github.issues.addLabels(context.issue({
+      labels: ['PR: awaiting review']
+    }))
+    const removeLabel = getRemoveLabel(context, 'PR: revision needed')
+    return Promise.all([addLabel, removeLabel])
+  })
+
+  // seems no event for convert_to_draft
+  // app.on(['pull_request.ready_for_review'], async context => {
+  //     return context.github.issues.addLabels(context.issue({
+  //         labels: ['PR: awaiting review']
+  //     }));
+  // });
+
+  app.on(['pull_request.edited'], async context => {
+    const addLabels = []
+    const removeLabels = []
+
+    // const isDraft = !!context.payload.pull_request.draft
+    // if (isDraft) {
+    //     removeLabels.push(getRemoveLabel(context, 'PR: awaiting review'));
+    // }
+    // else {
+    //     addLabels.push('PR: awaiting review');
+    // }
+
+    const content = context.payload.pull_request.body
+    if (content && content.indexOf('[x] The API has been changed.') > -1) {
+      addLabels.push('PR: awaiting doc')
+    } else {
+      removeLabels.push(getRemoveLabel(context, 'PR: awaiting doc'))
+    }
+
+    const defaultBranch = context.payload.repository.default_branch
+    const base = context.payload.pull_request.base
+    const changedBase = context.payload.changes.base
+    if (changedBase && changedBase.ref.from === defaultBranch && base.ref !== defaultBranch) {
+      addLabels.push('Branch: ' + base.ref)
+    }
+    if (changedBase && changedBase.ref.from !== defaultBranch) {
+      removeLabels.push(getRemoveLabel(context, 'Branch: ' + changedBase.ref.from))
+    }
+
+    const addLabel = context.github.issues.addLabels(context.issue({
+      labels: addLabels
+    }))
+
+    return Promise.all(removeLabels.concat([addLabel]))
+  })
+
+  app.on(['pull_request.closed'], async context => {
+    const actions = [
+      getRemoveLabel(context, 'PR: revision needed'),
+      getRemoveLabel(context, 'PR: awaiting review')
+    ]
+    const isMerged = context.payload.pull_request.merged
+    if (isMerged) {
+      const comment = context.github.issues.createComment(context.issue({
+        body: text.PR_MERGED
+      }))
+      actions.push(comment)
+    }
+    return Promise.all(actions)
+  })
+
+  app.on(['pull_request_review.submitted'], async context => {
+    if (context.payload.review.state === 'changes_requested' &&
+            isCoreCommitter(context.payload.review.user.login)
+    ) {
+      const addLabel = context.github.issues.addLabels(context.issue({
+        labels: ['PR: revision needed']
+      }))
+
+      const removeLabel = getRemoveLabel(context, 'PR: awaiting review')
+      return Promise.all([addLabel, removeLabel])
+    }
+  })
 }
 
-function replaceAll(str, search, replacement) {
-    return str.replace(new RegExp(search, 'g'), replacement);
+function getRemoveLabel (context, name) {
+  return context.github.issues.removeLabel(
+    context.issue({
+      name: name
+    })
+  ).catch(err => {
+    // Ignore error caused by not existing.
+    // if (err.message !== 'Not Found') {
+    //     throw(err);
+    // }
+  })
 }
 
-function isCommitter(auth) {
-    return auth === 'COLLABORATOR' || auth === 'MEMBER' || auth === 'OWNER';
+function closeIssue (context) {
+  const closeIssue = context.github.issues.update(context.issue({
+    state: 'closed'
+  }))
+  return closeIssue
+}
+
+function commentIssue (context, commentText) {
+  const comment = context.github.issues.createComment(context.issue({
+    body: commentText
+  }))
+  return comment
+}
+
+function replaceAll (str, search, replacement) {
+  return str.replace(new RegExp(search, 'g'), replacement)
+}
+
+function isCommitter (auth) {
+  return auth === 'COLLABORATOR' || auth === 'MEMBER' || auth === 'OWNER'
 }
--- src/text.js
+++ src/text.js
@@ -9,10 +9,10 @@ If you think you have already made your point clear without the template, or you
 为此带来的麻烦我深表歉意，但是请理解这是为了节约社区维护者的时间，以更高效地服务社区的开发者群体。
 如果您愿意，请使用 issue 模板重新创建 issue。
 
-如果您认为虽然没有使用模板，但您已经提供了复现问题的充分描述，或者您的问题无法使用模板表达，也可以重新 open 这个 issue。`;
+如果您认为虽然没有使用模板，但您已经提供了复现问题的充分描述，或者您的问题无法使用模板表达，也可以重新 open 这个 issue。`
 
 const ISSUE_CREATED =
-    `Hi! We\'ve received your issue and please be patient to get responded. 🎉
+    `Hi! We've received your issue and please be patient to get responded. 🎉
 The average response time is expected to be within one day for weekdays.
 
 In the meanwhile, please make sure that **you have posted enough image to demo your request**. You may also check out the [API](http://echarts.apache.org/api.html) and [chart option](http://echarts.apache.org/option.html) to get the answer.
@@ -21,46 +21,45 @@ If you don't get helped for a long time (over a week) or have an urgent question
 
 If you are interested in the project, you may also subscribe our [mailing list](https://echarts.apache.org/en/maillist.html).
 
-Have a nice day! 🍵`;
+Have a nice day! 🍵`
 
 const INACTIVE_ISSUE =
-    `This issue is closed due to not being active. Please feel free to open it again (for the author) or create a new one and reference this (for others) if you have further questions.`;
+    'This issue is closed due to not being active. Please feel free to open it again (for the author) or create a new one and reference this (for others) if you have further questions.'
 
 const ISSUE_UPDATED =
-    `An update has been made to this issue. The maintainers are right on their way. 🚒`;
+    'An update has been made to this issue. The maintainers are right on their way. 🚒'
 
 const ISSUE_TAGGED_WAITING_AUTHOR =
     `This issue is labeled with \`waiting-for: author\`.
-AT_ISSUE_AUTHOR Please give more information to get the help from the community, or close this issue if you think your problem has been fixed.`;
+AT_ISSUE_AUTHOR Please give more information to get the help from the community, or close this issue if you think your problem has been fixed.`
 
 const ISSUE_TAGGED_EASY =
     `This issue is labeled with \`difficulty: easy\`.
 AT_ISSUE_AUTHOR Would you like to debug it by yourself? This is a quicker way to get your problem fixed. Or you may wait for the community to fix.
 
-Please have a look at [How to debug ECharts](https://github.com/apache/incubator-echarts/blob/master/CONTRIBUTING.md#how-to-debug-echarts) if you'd like to give a try. 🤓`;
+Please have a look at [How to debug ECharts](https://github.com/apache/incubator-echarts/blob/master/CONTRIBUTING.md#how-to-debug-echarts) if you'd like to give a try. 🤓`
 
 const MISSING_DEMO =
-    `AT_ISSUE_AUTHOR Please provide a demo for the issue either with https://codepen.io/Ovilia/pen/dyYWXWM or https://gallery.echartsjs.com/editor.html.`;
+    'AT_ISSUE_AUTHOR Please provide a demo for the issue either with https://codepen.io/Ovilia/pen/dyYWXWM or https://gallery.echartsjs.com/editor.html.'
 
 const ISSUE_TAGGED_PRIORITY_HIGH =
-    `This issue is labeled with \`priority: high\`, which means it's a frequently asked problem and we will fix it ASAP.`;
-
+    'This issue is labeled with `priority: high`, which means it\'s a frequently asked problem and we will fix it ASAP.'
 
 const PR_OPENED =
     `Thanks for your contribution!
-The community will review it ASAP. In the meanwhile, please checkout [the coding standard](https://echarts.apache.org/en/coding-standard.html) and Wiki about [How to make a pull request](https://github.com/apache/incubator-echarts/wiki/How-to-make-a-pull-request).`;
+The community will review it ASAP. In the meanwhile, please checkout [the coding standard](https://echarts.apache.org/en/coding-standard.html) and Wiki about [How to make a pull request](https://github.com/apache/incubator-echarts/wiki/How-to-make-a-pull-request).`
 
 const PR_OPENED_BY_COMMITTER = PR_OPENED + `
 
-The pull request is marked to be \`PR: author is committer\` because you are a committer of this project.`;
+The pull request is marked to be \`PR: author is committer\` because you are a committer of this project.`
 
 const PR_AWAITING_DOC =
-    `Document changes are required in this PR. Please also make a PR to [apache/incubator-echarts-doc](https://github.com/apache/incubator-echarts-doc) for document changes. When the doc PR is merged, the maintainers will remove the \`PR: awaiting doc\` label.`;
+    'Document changes are required in this PR. Please also make a PR to [apache/incubator-echarts-doc](https://github.com/apache/incubator-echarts-doc) for document changes. When the doc PR is merged, the maintainers will remove the `PR: awaiting doc` label.'
 
 const PR_MERGED =
-    `Congratulations! Your PR has been merged. Thanks for your contribution! 👍`;
+    'Congratulations! Your PR has been merged. Thanks for your contribution! 👍'
 
-const PR_NOT_MERGED = `I'm sorry your PR didn't get merged. Don't get frustrated. Maybe next time. 😛`
+const PR_NOT_MERGED = 'I\'m sorry your PR didn\'t get merged. Don\'t get frustrated. Maybe next time. 😛'
 
 const LABEL_HOWTO =
     `Sorry, but please ask *how-to* questions on [Stack Overflow](https://stackoverflow.com/questions/tagged/echarts) or [segmentfault（中文）](https://segmentfault.com/t/echarts). You may also send an email about your question to dev@echarts.apache.org if you like.
@@ -79,18 +78,18 @@ For (1), we have decided to use the GitHub issue lists exclusively for work that
 For (2), we have found that issues that do not provide proper information upfront usually results in terribly inefficient back-and-forth communication just to extract the basic information needed for actual triaging. This is exactly why we have created this app: to ensure that every issue is created with the necessary information, and to save time on both sides.`
 
 module.exports = {
-    NOT_USING_TEMPLATE,
-    ISSUE_CREATED,
-    ISSUE_UPDATED,
-    MISSING_DEMO,
-    INACTIVE_ISSUE,
-    PR_OPENED,
-    LABEL_HOWTO,
-    PR_MERGED,
-    PR_OPENED_BY_COMMITTER,
-    PR_NOT_MERGED,
-    PR_AWAITING_DOC,
-    ISSUE_TAGGED_WAITING_AUTHOR,
-    ISSUE_TAGGED_EASY,
-    ISSUE_TAGGED_PRIORITY_HIGH
-};
+  NOT_USING_TEMPLATE,
+  ISSUE_CREATED,
+  ISSUE_UPDATED,
+  MISSING_DEMO,
+  INACTIVE_ISSUE,
+  PR_OPENED,
+  LABEL_HOWTO,
+  PR_MERGED,
+  PR_OPENED_BY_COMMITTER,
+  PR_NOT_MERGED,
+  PR_AWAITING_DOC,
+  ISSUE_TAGGED_WAITING_AUTHOR,
+  ISSUE_TAGGED_EASY,
+  ISSUE_TAGGED_PRIORITY_HIGH
+}
